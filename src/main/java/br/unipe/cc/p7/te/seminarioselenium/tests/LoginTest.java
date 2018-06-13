bb75d02b3ae7e17300f6d5c70e518449c6057ed2
Larissa Targino <l.targino@hotmail.com>
2018-06-13 19:54:34 -0300

    Adicionado o teste de Login para o website de tickets da Engeselt

--- src/main/java/br/unipe/cc/p7/te/seminarioselenium/tests/LoginTest.java
+++ src/main/java/br/unipe/cc/p7/te/seminarioselenium/tests/LoginTest.java
@@ -1,6 +1,39 @@
 package br.unipe.cc.p7.te.seminarioselenium.tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import br.unipe.cc.p7.te.seminarioselenium.utils.Constants;
+
 public class LoginTest extends TestClass {
 
+	@Test
+	public void RunLoginTestSucess() {
+		
+		//Efetua Login
+		String inputUserID = "name";
+		String inputPasswordID = "pass";
+		String inputLoginButtonXPath = "//*[@id=\"pwd-container\"]/div[2]/section/form/fieldset/input[3]";
+		
+		this.driver.get(Constants.Helpdesk_Engeselt_URL);
+		
+		WebElement inputUser = this.driver.findElement(By.id(inputUserID));
+		WebElement inputPassword = this.driver.findElement(By.id(inputPasswordID));
+		WebElement inputLoginButton = this.driver.findElement(By.xpath(inputLoginButtonXPath));
+		
+		inputUser.sendKeys("larissa.targino");
+		inputPassword.sendKeys("lary7206");
+		inputLoginButton.click();
+		
+		//Validar Login
+		String labelNameXPath = "/html/body/div[1]/nav/div/strong";
+		
+		WebElement labelName = this.driver.findElement(By.xpath(labelNameXPath));
+		
+		Assert.assertEquals("Larissa", labelName.getText());
+		
+	}
 	
 }
--- src/main/java/br/unipe/cc/p7/te/seminarioselenium/utils/Constants.java
+++ src/main/java/br/unipe/cc/p7/te/seminarioselenium/utils/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
 	public final static String WebSite_URL = "";
 	public final static String Google_URL = "https://www.google.com/";
+	public final static String Helpdesk_Engeselt_URL = "http://helpdesk.engeselt.com.br/scp/";
 	
 }
